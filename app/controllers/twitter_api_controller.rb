4bece9d5fc300b6d95182c720a4e1ac7e191796f
Jordane Giuly <jordane.giuly@gmail.com>
2014-02-28 11:51:05 +0100

    home page

--- app/controllers/twitter_api_controller.rb
+++ app/controllers/twitter_api_controller.rb
@@ -4,6 +4,7 @@ class TwitterApiController < ApplicationController
   end
   
   def search
+    @search_tag = params[:search_tag]
     @tweets = Tweet.stream
     @links = Tweet.link_list(@tweets)
     @tags = Tweet.tag_list(@tweets)
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ Project::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   get "twitter_api/index"
-
+  get "twitter_api/search"
+  
   root 'twitter_api#index'
   # See how all your routes lay out with "rake routes"
 
